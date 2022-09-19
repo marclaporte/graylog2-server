e8cb56848db2cb89edf8d4351aebef92765386a1
Mohamed Ould Hocine <mohamed.ould.hocine@graylog.com>
2022-09-19 16:22:53 +0200

    disable eslint errors

--- graylog2-web-interface/src/components/sidecars/administration/CollectorsAdministration.jsx
+++ graylog2-web-interface/src/components/sidecars/administration/CollectorsAdministration.jsx
@@ -52,6 +52,7 @@ const DisabledCollector = styled.div(({ theme }) => css`
 `);
 
 const CollectorsAdministration = createReactClass({
+  // eslint-disable-next-line react/no-unused-class-component-methods
   propTypes: {
     sidecarCollectorPairs: PropTypes.array.isRequired,
     collectors: PropTypes.array.isRequired,
@@ -120,6 +121,7 @@ const CollectorsAdministration = createReactClass({
     return selected.filter((sidecarCollectorId) => filteredSidecarCollectorIds.includes(sidecarCollectorId));
   },
 
+  // eslint-disable-next-line react/sort-comp
   handleConfigurationChange(selectedConfigurations, doneCallback) {
     const { selected, enabledCollectors } = this.state;
     const { onConfigurationChange } = this.props;
@@ -145,6 +147,7 @@ const CollectorsAdministration = createReactClass({
     onProcessAction(action, selectedCollectors, doneCallback);
   },
 
+  // eslint-disable-next-line react/no-unstable-nested-components
   formatHeader(selectedSidecarCollectorPairs) {
     const { collectors, configurations } = this.props;
     const { selected, enabledCollectors } = this.state;
@@ -212,6 +215,7 @@ const CollectorsAdministration = createReactClass({
     this.setState({ selected: newSelection });
   },
 
+  // eslint-disable-next-line react/no-unstable-nested-components
   formatSidecarNoCollectors(sidecar) {
     return (
       <ControlledTableList.Item key={`sidecar-${sidecar.node_id}`}>
@@ -236,6 +240,7 @@ const CollectorsAdministration = createReactClass({
     );
   },
 
+  // eslint-disable-next-line react/no-unstable-nested-components
   formatCollector(sidecar, collector, configurations) {
     const sidecarCollectorId = this.sidecarCollectorId(sidecar, collector);
     const configAssignmentIDs = sidecar.assignments.filter((assignment) => assignment.collector_id === collector.id).map((assignment) => assignment.configuration_id);
@@ -286,6 +291,7 @@ const CollectorsAdministration = createReactClass({
     );
   },
 
+  // eslint-disable-next-line react/no-unstable-nested-components
   formatSidecar(sidecar, collectors, configurations) {
     if (collectors.length === 0) {
       return this.formatSidecarNoCollectors(sidecar);
